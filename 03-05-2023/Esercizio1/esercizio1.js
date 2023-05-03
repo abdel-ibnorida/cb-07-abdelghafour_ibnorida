d1914f867f12d05a9f1c4d20679df66ca5fe53de
abdelibnorida <abdel.ibnorida@kappas.it>
2023-05-03 15:20:57 +0200

    Update exercise 1 and exercise 2

---
+++ 03-05-2023/Esercizio1/esercizio1.js
@@ -0,0 +1,6 @@
+let number_input = parseInt(prompt("Inserisci un numero:"));
+if (number_input % 2 === 0) {
+    console.log("Il numero inserito è pari");
+} else {
+    console.log("Il numero inserito è dispari");
+}
---
+++ 03-05-2023/Esercizio2/esercizio2.js
@@ -0,0 +1,2 @@
+let number_input = parseInt(prompt("Inserisci un numero:"));
+number_input % 2 === 0 ? (console.log("numero pari")) : (console.log("numero dispari"));
