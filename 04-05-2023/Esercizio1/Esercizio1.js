1981285a49462e7fbd4fe9673a3dda9eaec4c68b
abdel-ibnorida <114391513+abdel-ibnorida@users.noreply.github.com>
2023-05-04 15:48:48 +0200

    Create Esercizio1.js

---
+++ 04-05-2023/Esercizio1/Esercizio1.js
@@ -0,0 +1,19 @@
+let array1 = [1,2,3,4,5,6,7,8,9,10];
+
+let containerA = 0;
+
+let containerB = 0;
+
+for (let i = 0; i < array1.length/2; i++) {
+
+    containerA = array1 [(array1.length - 1 - i)];
+
+    containerB = array1 [i];
+
+    array1 [i] = containerA;
+
+    array1 [(array1.length - 1 - i)] = containerB;
+
+}
+
+console.log (array1);
