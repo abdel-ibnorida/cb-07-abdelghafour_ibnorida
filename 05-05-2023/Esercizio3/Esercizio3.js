fbf59e17bb7bc516c79066a553ed2d64c12680df
abdelibnorida <abdel.ibnorida@kappas.it>
2023-05-05 17:43:08 +0200

    add exercise 3

---
+++ 05-05-2023/Esercizio3/Esercizio3.js
@@ -0,0 +1,14 @@
+const person = {
+    name: "abdel",
+    surname: "ibnorida",
+    address: {
+        city: "bologna",
+        way: "delle belle arti",
+        cap: "40127",
+    },
+    sports: ["box", "golf", "tennis"],
+    age: 27,
+    nationality: "moroccan",
+  };
+
+  console.log(person);
