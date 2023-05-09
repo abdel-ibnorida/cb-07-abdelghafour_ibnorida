fc75e44caba0e0947d98475f1f2785411cfd2520
abdelibnorida <abdel.ibnorida@kappas.it>
2023-05-09 17:52:28 +0200

    add exercise 3

---
+++ 09-05-2023/Esercizio 3/script.js
@@ -0,0 +1,12 @@
+const input = document.querySelector('.input')
+const list = document.querySelector('.list')
+const button = document.querySelector('.button1')
+
+function addListElement() {
+    const listItem = document.createElement('li');
+    listItem.textContent = input.value;
+    list.appendChild(listItem);
+    input.value = null
+}
+
+button.addEventListener('click', addListElement)
