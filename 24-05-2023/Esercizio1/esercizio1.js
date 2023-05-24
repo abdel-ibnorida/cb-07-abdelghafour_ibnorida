3d5846b22af51a8080b8aa746f2063b17ba4208f
abdelibnorida <abdel.ibnorida@kappas.it>
2023-05-24 18:48:49 +0200

    add 24-05-2023

---
+++ 24-05-2023/Esercizio1/esercizio1.js
@@ -0,0 +1,6 @@
+let person = ["Mario", "Rossi", "Italia"];
+
+let [firstName, surname, nation] = person;
+
+console.log(person);
+console.log(firstName,surname,nation);
---
+++ 24-05-2023/Esercizio2/esercizio2.js
@@ -0,0 +1,11 @@
+let book = {
+titolo: "Il Nome della Rosa",
+autore: "Umberto Eco",
+anno: 1980
+};
+
+let {titolo, autore, anno} = book;
+
+console.log(book);
+
+console.log(autore, titolo, anno);
