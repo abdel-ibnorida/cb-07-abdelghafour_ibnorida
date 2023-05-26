4ae7f996aed4451abbfc6c4d7f393a0108ff5f78
abdelibnorida <abdel.ibnorida@kappas.it>
2023-05-26 18:45:10 +0200

    add 26-05-2023

---
+++ 26-05-2023/utils/fn.js
@@ -0,0 +1,121 @@
+
+export const cE = (el) => document.createElement(el);
+
+export const qS = (el) => document.querySelector(el);
+
+export const qSA = (els) => document.querySelectorAll(els);
+
+
+export const CreateTodoUserInterface = (data, loginData, divFather) => {
+    if (data.length > 0) {
+        const divUserUI = cE('div');
+        const usernameContainer = cE('div');
+        const userTodoContainer = cE('div');
+        const userP = cE('p');
+        const addTodoContainer = cE('div');
+        const addInputTodoText = cE('input');
+        const addTodoButton = cE('button');
+        let todoText = "";
+
+        divUserUI.className = "div_user_ui";
+        usernameContainer.className = "div_username";
+        userTodoContainer.className = "div_todo";
+        userP.className = "user_p";
+        addTodoContainer.className = "div_add_todo";
+        addTodoButton.className = "button_add_todo";
+
+        userP.textContent = "Username: " + loginData.username;
+        data.forEach(element => {
+            const singleTodoCartContainer = cE('div');
+            const deleteTodoButton = cE('button');
+            const changeStatusButton = cE('button');
+            const todoCard = cE('div');
+
+            singleTodoCartContainer.className = "single_todo_container";
+            todoCard.className = "todo_card";
+
+            todoCard.setAttribute("id", element.id);
+            todoCard.setAttribute("completed", element.completed);
+            todoCard.textContent = element.todo;
+            if (todoCard.element == false)
+            {
+                todoCard.className = "todo_card_notcompleted";
+            }
+            
+            deleteTodoButton.textContent = "Delete";
+            changeStatusButton.textContent = "Change Status";
+
+            singleTodoCartContainer.append(todoCard, deleteTodoButton,changeStatusButton);
+            userTodoContainer.append(singleTodoCartContainer);
+
+            deleteTodoButton.addEventListener('click', (e) => {
+                console.log(todoCard.id);
+                data.splice(data.findIndex((element) => element.id == todoCard.id), 1);
+                localStorage.setItem("todoList", JSON.stringify(data));
+                divFather.textContent = "";
+                CreateTodoUserInterface(JSON.parse(localStorage.getItem("todoList")), loginData, divFather);
+            });
+
+            changeStatusButton.addEventListener('click', (e) => {
+                console.log(todoCard);
+                console.log(todoCard.completed);
+                console.log(todoCard.id);
+            })
+        });
+        
+        addTodoButton.textContent = "Add new todo";
+        usernameContainer.append(userP);
+        addTodoContainer.append(addInputTodoText, addTodoButton);
+        divUserUI.append(usernameContainer, userTodoContainer, addTodoContainer);
+        divFather.append(divUserUI);
+
+        addInputTodoText.addEventListener('input', (e) => {
+            todoText = e.target.value;
+        })
+        addTodoButton.addEventListener('click', () => {
+            data.push({
+                "id": Date.now(),
+                "todo": todoText,
+                "completed": false,
+                "userId": loginData.id,
+            });
+            localStorage.setItem("todoList", JSON.stringify(data));
+            divFather.textContent = "";
+            CreateTodoUserInterface(JSON.parse(localStorage.getItem("todoList")), loginData, divFather);
+        })
+    }
+    else {
+        const divUserUI = cE('div');
+        const messageEmpyTodo = cE('p');
+        messageEmpyTodo.textContent = "There are not Todo, you can add some todo using the bottom form";
+        const addTodoContainer = cE('div');
+        const addInputTodoText = cE('input');
+        const addTodoButton = cE('button');
+        let todoText = "";
+
+        divUserUI.className = "div_user_ui";
+        messageEmpyTodo.className = "alert_p"
+        addTodoContainer.className = "div_add_todo";
+        addTodoButton.className = "button_add_todo";
+        addTodoButton.textContent = "Add new todo";
+
+        addTodoContainer.append(addInputTodoText, addTodoButton);
+        divUserUI.append(messageEmpyTodo, addTodoContainer);
+        divFather.append(divUserUI);
+
+        addInputTodoText.addEventListener('input', (e) => {
+            todoText = e.target.value;
+        })
+        addTodoButton.addEventListener('click', () => {
+            data.push({
+                "id": Date.now(),
+                "todo": todoText,
+                "completed": false,
+                "userId": loginData.id,
+            });
+            localStorage.setItem("todoList", JSON.stringify(data));
+            divFather.textContent = "";
+            CreateTodoUserInterface(JSON.parse(localStorage.getItem("todoList")), loginData, divFather);
+        })
+    }
+}
